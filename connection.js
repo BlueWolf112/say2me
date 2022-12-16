adaf9511ee1511832d4eeb878048090a5244687a
Yusuf <yusuffpi935@gmail.com>
2022-12-16 23:03:53 +0700

    major update with pc

---
+++ connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql")
+// route to server
+var hostname = "ru1.h.filess.io";
+var database = "say2me_sticklife";
+var port = "3306";
+var username = "say2me_sticklife";
+var password = "fd60b6cb436e8be5bcba5c3e29251c5932cc9807";
+
+const db = mysql.createConnection({
+	host: hostname,
+  user: username,
+  password,
+  database,
+  port,
+})
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,44 @@
+// Require source
+const express = require("express")
+const bodyParser = require("body-parser")
+const db = require('./connection')
+const app = express()
+const toHost = 3515;
+
+//connect to other resouces
+app.use('/public', express.static('public'))
+app.use('/sent', express.static('sent'))
+app.use(bodyParser.urlencoded({ extended: true }))
+// set file html
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+// Connecting to others
+db.connect((err) => {
+	if (err) throw err
+	console.log("Database Connected to Host " + toHost);
+	//menampilkan input (main) page
+	app.get("/", (req,res) => {
+	res.render("index");
+	})
+	//untuk insert data di input (main) page
+	app.post("/sent", (req, res) => {
+		const insertSql = `INSERT INTO users (message) VALUES ('${req.body.pesan}');`
+		db.query(insertSql, (err, result) => {
+			if (err) throw err
+				res.redirect("/sent")
+		})
+	})
+	//menampilkan hasil data
+	app.get("/analytic", (req, res) => {
+		const sql = "SELECT * FROM `users`"
+		db.query(sql, (err, result) => {
+			const users = JSON.parse(JSON.stringify(result))
+			res.render("analytic/index", {users: users, title: "Hasil Respon User"});
+		})
+	})
+})
+//set listening
+app.listen(toHost, () => {
+	console.log("Preparing to connect database...");
+});
