47579aa1ba7fe3699a96eace944197854d077b23
Yusuf <yusuffpi935@gmail.com>
2022-12-29 01:29:46 +0700

    major update. penambahan repon berdasarkan id.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const db = require('./connection')
 const app = express()
-const toHost = 3515;
+const PORT = process.env.PORT || 3515;
 
 //connect to other resouces
 app.use('/public', express.static('public'))
@@ -16,11 +16,12 @@ app.set("views", "views")
 // Connecting to others
 db.connect((err) => {
 	if (err) throw err
-	console.log("Database Connected to Host " + toHost);
+	console.log("Database Connected to Host " + PORT);
 	//menampilkan input (main) page
 	app.get("/", (req,res) => {
-	res.render("index");
+		res.render("index");
 	})
+
 	//untuk insert data di input (main) page
 	app.post("/sent", (req, res) => {
 		const insertSql = `INSERT INTO users (message) VALUES ('${req.body.pesan}');`
@@ -29,6 +30,7 @@ db.connect((err) => {
 				res.redirect("/sent")
 		})
 	})
+
 	//menampilkan hasil data
 	app.get("/analytic", (req, res) => {
 		const sql = "SELECT * FROM `users`"
@@ -37,8 +39,26 @@ db.connect((err) => {
 			res.render("analytic/index", {users: users, title: "Hasil Respon User"});
 		})
 	})
+
+	//menampilkan data spesifik berdasarkan id
+	app.get("/analytic/:id", (req, res) => {
+		const id = req.params.id;
+		//get specific data using querry sql
+		const sqlById = "SELECT `message` FROM `users` WHERE `id` = " + id;
+		const params = [id];
+		db.query(sqlById, params, (err, rows) => {
+			if (err) throw err
+			users = rows[0];
+			const rusak = "Data respon tidak ditemukan.";
+			if (users == undefined) {
+				res.render("analytic/specific", {users:rusak});
+			} else {
+				res.render("analytic/specific", {users:users});
+			}
+		})
+	})
 })
 //set listening
-app.listen(toHost, () => {
+app.listen(PORT, () => {
 	console.log("Preparing to connect database...");
 });
