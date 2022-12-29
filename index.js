982913f00d5cbc1e27b27569f5f83c9c6f91871f
BlueWolf112 <78911105+BlueWolf112@users.noreply.github.com>
2022-12-29 16:55:36 +0700

    add __dirname + '/../views'

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/sent', express.static('sent'))
 app.use(bodyParser.urlencoded({ extended: true }))
 // set file html
 app.set("view engine", "ejs")
-app.set("views", "views")
+app.set("views", __dirname + "/../views")
 
 // Connecting to others
 db.connect((err) => {
