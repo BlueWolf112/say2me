a7258d1d92e84c040fb9fddbac1a199603af305b
Yusuf Fahar <yusuffpi935@gmail.com>
2023-07-18 00:43:38 +0700

    Memperbarui database dan tampilan spesifik

--- connection.js
+++ connection.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql")
 // route to server
-var hostname = "ru1.h.filess.io";
-var database = "say2me_sticklife";
-var port = "3306";
-var username = "say2me_sticklife";
-var password = "fd60b6cb436e8be5bcba5c3e29251c5932cc9807";
+var hostname = "jyi.h.filess.io";
+var database = "say2me_bordermore";
+var port = "3307";
+var username = "say2me_bordermore";
+var password = "4f5927e0a4b2c79991fe3b9a97a2b5836e2de607";
 
 const db = mysql.createConnection({
-	host: hostname,
+  host: hostname,
   user: username,
   password: password,
   database: database,
--- index.js
+++ index.js
@@ -24,10 +24,12 @@ db.connect((err) => {
 
 	//untuk insert data di input (main) page
 	app.post("/sent", (req, res) => {
-		const insertSql = `INSERT INTO users (message) VALUES ('${req.body.pesan}');`;
+		const waktu = new Date().toLocaleString();
+		const pesan = req.body.pesan;
+		const insertSql = `INSERT INTO users (time, message) VALUES ('${waktu}', '${pesan}');`;
 		db.query(insertSql, (err, result) => {
 			if (err) throw err
-				res.redirect("/sent")
+			res.redirect("/sent")
 		});
 	});
 
@@ -36,7 +38,7 @@ db.connect((err) => {
 		const sql = "SELECT * FROM `users`";
 		db.query(sql, (err, result) => {
 			const users = JSON.parse(JSON.stringify(result))
-			res.render("analytic/index", {users: users, title: "Hasil Respon User"});
+			res.render("analytic/index", { users: users, title: "Hasil Respon User" });
 		});
 	});
 
@@ -51,9 +53,9 @@ db.connect((err) => {
 			users = rows[0];
 			const rusak = "Data respon tidak ditemukan.";
 			if (users == undefined || null) {
-				res.render("analytic/specific", {users:rusak});
+				res.render("analytic/specific", { users: rusak });
 			} else {
-				res.render("analytic/specific", {users:users});
+				res.render("analytic/specific", { users: users });
 			}
 		});
 	});
